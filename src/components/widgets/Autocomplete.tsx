73cab93dbd98af638d9a0d66557b28b99452ff8b
Ivan Mironchik <me.roaiven@gmail.com>
2017-01-01 14:35:55 +0300

    Initial commit

---
+++ src/components/widgets/Autocomplete.tsx
@@ -0,0 +1,141 @@
+import * as React from 'react';
+
+import Rating from '../Rating';
+
+function Autocomplete({
+  searchSuggestions,
+  productMatches,
+  onSuggestionClick,
+  onProductClick,
+  direction,
+  suggestionsTitle,
+  productMatchesTitle,
+  tipTitle,
+  query,
+}: Props) {
+  const searchSuggestionsNode = (
+    <div className="findify-search-suggestions">
+      {suggestionsTitle && (
+        <span className="findify-search-suggestions__title">
+          {suggestionsTitle}
+        </span>
+      )}
+      <div className="findify-search-suggestions__list">
+        {searchSuggestions.map((s: Suggestion, i: number) => (
+          <span
+            key={i}
+            className="findify-search-suggestions__suggestion"
+            onClick={() => onSuggestionClick(s.query)}
+          >
+            {s.title}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+
+  const productMatchesNode = (
+    <div className="findify-product-matches">
+      {productMatchesTitle && (
+        <span className="findify-product-matches__title">
+          {productMatchesTitle}
+        </span>
+      )}
+      <div className="findify-product-matches">
+        {productMatches.map((p: ProductMatch, i: number) => (
+          <a
+            className="findify-product-matches__item"
+            href={p.link}
+          >
+            <div className="findify-product-matches__image">
+              <img
+                className="findify-product-matches__image-link"
+                src={p.imageLink}
+              />
+            </div>
+            <span className="findify-product-matches__title">
+              {p.title}
+            </span>
+            <div className="findify-product-matches__rating">
+              <Rating
+                value={p.rating}
+                reviewsCount={p.reviewsCount}
+              />
+            </div>
+            <div className="findify-product-matches__price">
+              {p.price}
+              {p.discountPrice && (
+                <div className="findify-product-matches__price-discount">
+                  {p.discountPrice}
+                </div>
+              )}
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="findify-autocomplete">
+      <div className="findify-autocomplete__body">
+        {direction === 'ltr' ? (
+          <div className="findify-autocomplete__search-suggestions">
+            {searchSuggestionsNode}
+          </div>
+          <div className="findify-autocomplete__product-matches">
+            {productMatchesNode}
+          </div>
+        ) : (
+          <div className="findify-autocomplete__product-matches">
+            {productMatchesNode}
+          </div>
+          <div className="findify-autocomplete__search-suggestions">
+            {searchSuggestionsNode}
+          </div>
+        )}
+      </div>
+      {tipTitle && query && (
+        <div className="findify-autocomplete__tip">
+          {tipTitle}
+          <span className="findify-autocomplete__query">
+            {query}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+type Props = {
+  searchSuggestions: Suggestion[],
+  productMatches: ProductMatch[],
+  onSuggestionClick: OnSuggestionClick,
+  onProductClick: OnProductMatchClick,
+  direction: 'ltr' | 'rtl',
+  suggestionsTitle?: string,
+  productMatchesTitle?: string,
+  tipTitle?: string,
+  query?: string,
+};
+
+type Suggestion = {
+  title: string,
+  query: string,
+};
+
+type ProductMatch = {
+  link: string,
+  imageLink: string,
+  title: string,
+  price: string,
+  discountPrice?: string,
+  reviewsCount?: string,
+  rating?: 1 | 2 | 3 | 4 | 5,
+};
+
+type OnSuggestionClick = (query: string) => void
+
+type OnProductMatchClick = (id: number) => void
+
+export default Autocomplete;
---
+++ src/components/widgets/Rating.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import * as times from 'lodash/times';
+
+function Rating({
+  value,
+  reviewsCount,
+}: Props) {
+  return (
+    <div className="findify-rating">
+      {times(5, (i: number) => i + 1).map((item: number) => {
+        const diff = item - value;
+        const modifier = diff === 0.5 ? 'half' : diff <== 0 'full' : 'empty';
+        return (
+          <span className={`findify-rating__star findify-rating__star_${modifier}`} />
+        );
+      })}
+      {reviewsCount && (
+        <span className="findify-rating__reviews-count">{reviewsCount}</span>
+      )}
+    </div>
+  );
+}
+
+type Props = {
+  value: 1 | 1.5 | 2 | 2.5 | 3 | 3.5 | 4 | 4.5 | 5,
+  reviewsCount?: string,
+};
+
+export default Rating;
