41abce9ca275b12932cf2bd8b7e737632deded35
Ivan Mironchik <me.roaiven@gmail.com>
2017-01-01 22:34:57 +0300

    Fix name

--- dev/src/containers/AutocompleteContainer.tsx
+++ dev/src/containers/AutocompleteContainer.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 
-import { Autocomplete } from '../../../src/components/widgets/Autocomplete';
+import { AutocompleteWidget } from '../../../src';
 
 function AutocompleteContainer() {
   return (
-    <Autocomplete
+    <AutocompleteWidget
       
     />
   );
