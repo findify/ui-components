cb0324e5bd72cf35771c959c4ef17f41c0b01d21
Ivan Mironchik <me.roaiven@gmail.com>
2017-01-01 22:52:33 +0300

    Provide props to container

--- dev/src/containers/AutocompleteContainer.tsx
+++ dev/src/containers/AutocompleteContainer.tsx
@@ -3,10 +3,76 @@ import * as React from 'react';
 import { AutocompleteWidget } from '../../../src';
 
 function AutocompleteContainer() {
+  const props = {
+    direction: 'ltr',
+    onSearchSuggestionClick: (query: string) => {},
+    productMatches: [{
+      link: '/test',
+      imageLink: 'http://placehold.it/300x300',
+      title: 'Test product',
+      price: 300,
+      discountPrice: 250,
+      reviewsCount: 3,
+      rating: 4,
+    }, {
+      link: '/test',
+      imageLink: 'http://placehold.it/300x300',
+      title: 'Test product',
+      price: 300,
+      discountPrice: 250,
+      reviewsCount: 3,
+      rating: 4,
+    }, {
+      link: '/test',
+      imageLink: 'http://placehold.it/300x300',
+      title: 'Test product',
+      price: 300,
+      discountPrice: 250,
+      reviewsCount: 3,
+      rating: 4,
+    }, {
+      link: '/test',
+      imageLink: 'http://placehold.it/300x300',
+      title: 'Test product',
+      price: 300,
+      discountPrice: 250,
+      reviewsCount: 3,
+      rating: 4,
+    }, {
+      link: '/test',
+      imageLink: 'http://placehold.it/300x300',
+      title: 'Test product',
+      price: 300,
+      discountPrice: 250,
+      reviewsCount: 3,
+      rating: 4,
+    }],
+    searchSuggestions: [{
+      title: 'Test suggestion',
+      query: 'test-suggestion',
+    }, {
+      title: 'Test suggestion 2',
+      query: 'test-suggestion-2',
+    }, {
+      title: 'Test suggestion 3',
+      query: 'test-suggestion-3',
+    }, {
+      title: 'Test suggestion 4',
+      query: 'test-suggestion-4',
+    }, {
+      title: 'Test suggestion 5',
+      query: 'test-suggestion-5',
+    }, {
+      title: 'Test suggestion-6',
+      query: 'test-suggestion-6',
+    }, {
+      title: 'Test suggestion-7',
+      query: 'test-suggestion-7',
+    }],
+  };
+
   return (
-    <AutocompleteWidget
-      
-    />
+    <AutocompleteWidget {...props} />
   );
 }
 
