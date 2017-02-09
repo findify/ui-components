22a050b7f9105d23f307e6fd9b9263592408e679
bitaru <bitaru@MacBook-Pro-Vitalij.local>
2017-02-09 20:01:17 +0300

    Add product card

---
+++ src/internals/Product/index.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { format as currencyFormat } from 'currency-formatter';
+
+import { RatingWidget } from 'widgets/Rating';
+
+const styles = require('./styles.css');
+
+
+const Title = ({ text }) => text &&
+  <h5 className={styles.title}>{text}</h5>
+
+const Description = ({ text }) => text &&
+  <p className={styles.description}>{text}</p>
+
+const Price = ({ price, compareAt, currency = 'USD' }) => price &&
+  <div className={styles.priceWrap}>
+    <span className={styles.price}>
+      { currencyFormat(price[0], { code: currency }) }
+    </span>
+    {
+      compareAt &&
+      <span className={styles.compare}>
+        { currencyFormat(compareAt, { code: currency }) }
+      </span>
+    }
+  </div>
+
+export const Product = ({
+  productUrl,
+  imageUrl,
+  description,
+  title,
+  reviews,
+  price,
+  compareAt,
+  ...rest
+}: any) => {
+  console.log(rest);
+  
+  return (
+    <a href={productUrl} className={styles.wrap}>
+      <div className={styles.imageWrap}>
+        <img className={styles.image} src={imageUrl} alt={title} />
+      </div>
+      <div className={styles.description}>
+        <Title text={title} />
+        <Description text={description} />
+      </div>
+      {
+        reviews &&
+        <div className={styles.rating}>
+          <RatingWidget reviewsCount={reviews.totalReviews} value={reviews.averageScore} />
+        </div>
+      }
+      {
+        price &&
+          <Price price={price} compareAt={compareAt}/>
+      }
+    </a>
+  )
+}
--- src/widgets/Rating/index.tsx
+++ src/widgets/Rating/index.tsx
@@ -1,33 +1,37 @@
+import { hasModifier } from 'tslint/lib';
 import * as React from 'react';
-import * as times from 'lodash/times';
+import * as cx from 'classnames';
+import { mapProps } from 'recompose';
 
-function RatingWidget({
+const styles = require('./styles.css');
+
+const Start = mapProps(({ diff }: any) => ({
+  iconClass: `fa-star${diff === 0.5 ? '-half-o' : diff <= 0 ? '' : '-o'}`
+}))
+(({ iconClass }: any) =>
+  <span className={cx(styles.star, iconClass, 'fa')} />
+)
+
+export const RatingWidget = (({
   value,
   reviewsCount,
-}: Props) {
-  return (
-    <div className="findify-rating-widget">
-      {times(5, (i: number) => i + 1).map((item: number, i: number) => {
-        const diff = item - value;
-        const modifier = diff === 0.5 ? 'half' : diff <= 0 ? 'full' : 'empty';
-        return (
-          <span
-            key={i}
-            className={`findify-rating-widget__star findify-rating-widget__star_${modifier}`}
-          />
-        );
-      })}
-      {reviewsCount && (
-        <span className="findify-rating-widget__reviews-count">{reviewsCount}</span>
-      )}
+}: Props) =>
+  <div className={styles.wrap}>
+    <div className={styles.stars}>
+      {
+        [...Array(5).keys()].map((item: number) =>  <Start key={item} diff={item - value} />)
+      }
     </div>
-  );
-}
+    {
+      reviewsCount && (
+        <span className={styles.count}>({reviewsCount})</span>
+      )
+    }
+  </div>
+);
 
 type Props = {
   value: number,
   // value: 1 | 1.5 | 2 | 2.5 | 3 | 3.5 | 4 | 4.5 | 5,
   reviewsCount?: number,
 };
-
-export { RatingWidget };
