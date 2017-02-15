fcd9e37e7df70498182269db3dd18b41648b36f6
Ivan Mironchik <me.roaiven@gmail.com>
2017-02-15 21:08:04 +0300

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 import './styles.global.css';
 
+export { Autocomplete } from './widgets/Autocomplete';
+export { BreadCrumbs } from './widgets/BreadCrumbs';
+export { CategoryFacet } from './widgets/CategoryFacet';
+export { CheckboxFacet } from './widgets/CheckboxFacet';
+export { ColorFacet } from './widgets/ColorFacet';
+export { Grid } from './widgets/Grid';
+export { Pagination } from './widgets/Pagination';
+export { Product } from './widgets/Product';
 export { ProductsCarousel } from './widgets/ProductsCarousel';
 export { ProductsGrid } from './widgets/ProductsGrid';
-export { Product } from './widgets/Product';
+export { RangeFacet } from './widgets/RangeFacet';
 export { Rating } from './widgets/Rating';
-export { Pagination } from './widgets/Pagination';
 export { Sorting } from './widgets/Sorting';
-export { Grid } from './widgets/Grid';
-
-
-export { Autocomplete } from './widgets/Autocomplete';
-
-
-// export { Product } from './internals/Product';
-
-// export { RangeFacet } from './widgets/RangeFacet';
-// export { CategoryFacet } from './widgets/CategoryFacet';
-// export { CheckboxFacet } from './widgets/CheckboxFacet';
-// export { ColorFacet } from './widgets/ColorFacet';
-// export { GenericFacet } from './widgets/GenericFacet';
+/* export { GenericFacet } from './widgets/GenericFacet'; */
