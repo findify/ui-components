f1ac8a43157667c68d90f61f225d4310eadffb07
Bitaru <i@bitaru.com>
2017-03-02 20:53:55 +0300

    Add host and facets

--- dev/stories/Pagination.tsx
+++ dev/stories/Pagination.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
-import { Pagination } from '../../src/widgets/Pagination';
 import { withKnobs, text, boolean, number } from '@kadira/storybook-addon-knobs';
 const { storiesOf, action } = require('@kadira/storybook');
+import { host } from 'storybook-host';
+
+import { Pagination } from '../../src/widgets/Pagination';
 
 storiesOf('Pagination', module)
   .addDecorator(withKnobs)
+  .addDecorator(host({
+    title: 'Pagination for products grid',
+    align: 'center top',
+  }))
+
   .addWithInfo('default', () => {
     const config = {
       i18n: {
--- dev/stories/Rating.tsx
+++ dev/stories/Rating.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
-import { Rating } from '../../src/widgets/Rating';
 import { withKnobs, text, boolean, number } from '@kadira/storybook-addon-knobs';
+import { host } from 'storybook-host';
 const { storiesOf, action } = require('@kadira/storybook');
+
+import { Rating } from '../../src/widgets/Rating';
 
 storiesOf('Rating', module)
   .addDecorator(withKnobs)
+  .addDecorator(host({
+    title: 'Star Rating component',
+    align: 'center top',
+  }))
   .addWithInfo('default', () => {
     return (
       <Rating
