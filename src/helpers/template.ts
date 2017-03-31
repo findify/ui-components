c30d7318d693ac66456b8985b6079594b31bf679
Bitaru <i@bitaru.com>
2017-03-31 12:25:12 +0300

    fix stickers

--- src/helpers/template.ts
+++ src/helpers/template.ts
@@ -1,6 +1,6 @@
 import { memoize } from 'lodash';
 
-export default (template, selector = /%s/g) => memoize((...args) => {
+export default (template = '', selector = /%s/g) => memoize((...args) => {
   let index = -1;
   return template.replace(selector, () => {
     index++;
