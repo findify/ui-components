b50d82ec9f256580024ca013b095e5fed0ab4283
Bitaru <i@bitaru.com>
2017-04-03 10:28:55 +0300

    Fix issues

--- src/helpers/template.ts
+++ src/helpers/template.ts
@@ -1,9 +1,12 @@
-import { memoize } from 'lodash';
+import { memoize, unescape } from 'lodash';
 
-export default (template = '', selector = /%s/g) => memoize((...args) => {
-  let index = -1;
-  return template.replace(selector, () => {
-    index++;
-    return args[index] || '';
-  });
-})
+export default (template = '', selector = /%s/g) => {
+  const tpl = unescape(template);
+  return memoize((...args) => {
+    let index = -1;
+    return tpl.replace(selector, () => {
+      index++;
+      return args[index] || '';
+    });
+  })
+}
