8f31bc66b3670ea97b5b976c7ed1c12c4a34b58f
Bitaru <i@bitaru.com>
2017-04-04 08:53:23 +0300

    Fix Range facet && Sorting

--- src/helpers/formatRange.ts
+++ src/helpers/formatRange.ts
@@ -1,6 +1,7 @@
 import { format } from 'currency-formatter';
+import { unescape } from 'lodash';
 
 export default ({ from, to, config: { currency, i18n } }) => 
   from && to && `${format(from, currency)} - ${format(to, currency)}` ||
-  from && !to && `${i18n.under} ${format(from, currency)}` ||
-  !from && to && `${i18n.up} ${format(to, currency)}`;
+  from && !to && `${unescape(i18n.up)} ${format(from, currency)}` ||
+  !from && to && `${unescape(i18n.under)} ${format(to, currency)}`;
