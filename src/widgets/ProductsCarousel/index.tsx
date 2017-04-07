6227e2fba6bf57b8f3c635b238545a153bce29e3
Bitaru <i@bitaru.com>
2017-04-07 15:57:57 +0300

    Slider items synamic count

--- src/widgets/ProductsCarousel/index.tsx
+++ src/widgets/ProductsCarousel/index.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
-import { compose, setDisplayName, mapProps, defaultProps } from 'recompose';
+import {
+  compose, setDisplayName, mapProps,
+  defaultProps, withPropsOnChange, pure,
+  createEagerElement
+} from 'recompose';
 import { Product } from 'widgets/Product';
 import * as cx from 'classnames';
 import withConfig from 'helpers/withConfig';
+import withHooks from 'helpers/withHooks';
 
 import './slider.global.css';
 import './slider-theme.global.css';
 
 const Slider = require('react-slick');
 const styles = require('./styles.css');
+
+const emptyObject = {};
 
 const Arrow = ({ dir, className, currentSlide, slideCount, ...props }: any) => (
   <button {...props} className={cx(styles.arrow, 'fa', `fa-chevron-${dir}`, className)} />
@@ -24,19 +31,31 @@ const sliderProps = {
   prevArrow: <Arrow dir='left' />
 };
 
+const countProductsToShow = (width) => {
+  if (width > 710) return 4;
+  if (width > 500) return 3
+  if (width > 290) return 2
+  return 1;
+};
+
 export const HOC = compose(
   setDisplayName('ProductsCarousel'),
-  withConfig({
-    productsToShow: 5
-  }),
+
+  withConfig({ productsToShow: 5 }),
+
+  withPropsOnChange(['node'],({ node, config }) => ({
+    slidesToShow: node && countProductsToShow(node.position.get('width')) || config.productsToShow
+  })),
+
+  withHooks('slider')
 );
 
 export const Component = (({
   items,
   onProductClick,
+  slidesToShow,
   config: {
-    productsToShow: slidesToShow,
-    slickSetting = {},
+    slickSetting = emptyObject,
     title,
     stickers,
     ...config
@@ -46,8 +65,13 @@ export const Component = (({
     { 
       title && <h4 className={styles.title}>{ title }</h4>
     }
-    <Slider {...{ ...sliderProps, ...slickSetting, slidesToShow }}>
-      { 
+    {
+      createEagerElement(Slider,
+        {
+          ...sliderProps,
+          ...slickSetting,
+          slidesToShow
+        },
         items.map(product =>
           <div key={product.id}>
             <Product
@@ -59,8 +83,8 @@ export const Component = (({
               }} />
           </div>
         )
-      }
-    </Slider>
+      )
+    }
   </div>
 ));
 
