5d0a69e425555b02f644f970419b2a4be55a7480
Bitaru <i@bitaru.com>
2017-04-10 11:05:47 +0300

    Fix range facet key

--- src/helpers/formatRange.ts
+++ src/helpers/formatRange.ts
@@ -3,5 +3,5 @@ import { unescape } from 'lodash';
 
 export default ({ from, to, config: { currency, i18n } }) => 
   from && to && `${format(from, currency)} - ${format(to, currency)}` ||
-  from && !to && `${unescape(i18n.up)} ${format(from, currency)}` ||
+  from && !to && `${format(from, currency)} ${unescape(i18n.up)}` ||
   !from && to && `${unescape(i18n.under)} ${format(to, currency)}`;
