0220851b39d82bd28889eb85552ba4600c1d804b
Bitaru <i@bitaru.com>
2017-04-25 16:44:11 +0300

    Check for price error

--- src/helpers/getPrice.ts
+++ src/helpers/getPrice.ts
@@ -1,13 +1,18 @@
+import { isArray } from 'lodash';
 import { format } from 'currency-formatter';
 
 export const getPrice = (price, currency) => {
-  if (typeof price === 'string') return price;
+  if (!isArray(price)) return price;
+
+  const ddd = price;
+  console.log(ddd);
+  
   if (!!price.reduce((a, b) => a === b ? a : NaN )) {
     return format(price[0], currency)
-  } else {
-    return [
-      format(Math.min.apply(Math,price), currency),
-      format(Math.max.apply(Math,price), currency),
-    ].join(' - ')
   }
+
+  return [
+    format(Math.min.apply(Math,price), currency),
+    format(Math.max.apply(Math,price), currency),
+  ].join(' - ');
 }
