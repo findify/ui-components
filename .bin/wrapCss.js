b9a83ed4ebd5fd78f56d4d4b696c4d57ff204f96
Bitaru <i@bitaru.com>
2017-06-01 14:56:38 +0300

    Add default color to frame-content

--- .bin/wrapCss.js
+++ .bin/wrapCss.js
@@ -5,7 +5,10 @@ const prefixwrap = require('postcss-wrap');
 
 new Promise((resolve) => {
   const file = fs.readFileSync(path.resolve(process.cwd(), 'dist/styles.css'));
-  postcss([prefixwrap({ selector: '.findify-root', skip: /.findify-root/ })])
+  postcss([
+    prefixwrap({ selector: '.findify-root', skip: ['.findify-root', '.frame-content'] }),
+    require('cssnano')({ preset: 'default' }),
+  ])
   .process(file)
   .then(result => {
     fs.writeFileSync(path.resolve(process.cwd(), 'dist/wrapped.css'), result.css);
