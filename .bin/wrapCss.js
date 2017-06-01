6a483f8ffa0d440f5a549d807b65354635c89dd7
Bitaru <i@bitaru.com>
2017-06-01 14:40:57 +0300

    Fix class skipping in postcss-wrap

--- .bin/wrapCss.js
+++ .bin/wrapCss.js
@@ -5,7 +5,7 @@ const prefixwrap = require('postcss-wrap');
 
 new Promise((resolve) => {
   const file = fs.readFileSync(path.resolve(process.cwd(), 'dist/styles.css'));
-  postcss([prefixwrap({ selector: '.findify-root', skip: '.findify-root' })])
+  postcss([prefixwrap({ selector: '.findify-root', skip: /.findify-root/ })])
   .process(file)
   .then(result => {
     fs.writeFileSync(path.resolve(process.cwd(), 'dist/wrapped.css'), result.css);
