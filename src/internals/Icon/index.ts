574a324074c12af98b708f342002442e5a641c0a
Bitaru <i@bitaru.com>
2017-06-20 12:45:39 +0300

    Fix icons

--- src/internals/Icon/index.ts
+++ src/internals/Icon/index.ts
@@ -1,26 +1,6 @@
-import { DOM } from 'react';
-import { compose, defaultProps, mapProps } from 'recompose';
+import { compose, defaultProps, mapProps, componentFromProp } from 'recompose';
+import { memoize } from 'lodash';
 
-
-// We can not use dynamic imports, coz we need to render
-// icons in initialHtml in iframe mode
-const icons = {
-  'arrow-down': require('./icons/arrow-down.svg').default,
-  'check-checked': require('./icons/check-checked.svg').default,
-  'check-empty': require('./icons/check-empty.svg').default,
-  'check': require('./icons/check.svg').default,
-  'chevron-down': require('./icons/chevron-down.svg').default,
-  'chevron-left': require('./icons/chevron-left.svg').default,
-  'chevron-right': require('./icons/chevron-right.svg').default,
-  'close-round': require('./icons/close-round.svg').default,
-  'cross': require('./icons/cross.svg').default,
-  'minus': require('./icons/minus.svg').default,
-  'plus': require('./icons/plus.svg').default,
-  'search': require('./icons/search.svg').default,
-  'star-empty': require('./icons/star-empty.svg').default,
-  'star-full': require('./icons/star-full.svg').default,
-  'star-half': require('./icons/star-half.svg').default,
-}
 
 const Icon: any = compose(
   defaultProps({
@@ -28,12 +8,11 @@ const Icon: any = compose(
     height: 16
   }),
   mapProps(({ name, ...rest }) => {
-    const { id, url } = icons[name];
     return {
       ...rest,
-      dangerouslySetInnerHTML: { __html: `<use xlink:href="${url ? url : '#' + id}"></use>` }
+      component: require(`!!babel-loader!react-svg-loader!./icons/${name}.svg`).default
     };
   })
-)(DOM.svg);
+)(componentFromProp('component'));
 
 export default Icon;
